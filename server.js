6f728fc6b3f15681922350fc6a11f84b6e5b8023
Hack Reactor Students <github@hackreactor.com>
2013-12-14 10:49:06 -0800

    basic server functionality

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var facts = require('facts');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('*', function(request, response){
+  response.send(facts[Math.floor(Math.random()*facts.length)]);
+});
+
+app.listen(port);
